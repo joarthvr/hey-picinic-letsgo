64d7a9338465203def013e381f5eedec3d1a16fb
Arthur Jo <ioi456859@gmail.com>
2024-09-30 09:06:14 +0900

    style: main page card

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,13 @@
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 import { Outlet } from 'react-router-dom';
+
 export default function MainLayout() {
   return (
     <>
-      <Header/>
+      <Header />
       <Outlet />
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,56 @@
+import { useMemo } from 'react';
 import { css, useTheme } from '@emotion/react';
 import { ThemeType } from '../assets/styles/theme';
 import HomeFestivalCard from '../components/home/HomeFestivalCard';
+import dummy from '../models/data.json';
+
+interface City {
+  id: number | string;
+  city: string;
+}
+
 const HomeStyles = (theme: ThemeType) => ({
   section: css({
     width: '100%',
-    height: '50vh',
+    minHeight: '50vh',
     fontFamily: 'Pretendard',
   }),
-  msg: {
-    textAlign: 'center' as const,
+  msg: css({
+    textAlign: 'center',
     fontSize: '2rem',
     fontStyle: 'normal',
     lineHeight: 'normal',
-  },
+  }),
   centerMsg: css({
     color: '#FFF',
     fontWeight: '300',
   }),
-  section1: {
+  section1: css({
     background: theme.colors.MAIN_BLUE,
     ...theme.interval.mainPage,
-  },
-  sec2H2: {
+  }),
+  sec2H2: css({
     color: '#000',
     fontWeight: '700',
     marginTop: '8.87rem',
-  },
+  }),
+  cards: css({
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '0.94rem',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '4.06rem',
+    paddingBottom: '6.06rem',
+    
+  }),
 });
+
 const Home = () => {
   const theme = useTheme() as ThemeType;
-  const styles = HomeStyles(theme);
+  const styles = useMemo(() => HomeStyles(theme), [theme]);
+  const cities: City[] = dummy.cities;
+
   return (
     <div>
       <section css={[styles.section, styles.section1]}>
@@ -43,9 +64,14 @@ const Home = () => {
         <h2 css={[styles.msg, styles.sec2H2]}>
           인기있는 지역의 행사를 확인해보세요!
         </h2>
-        <HomeFestivalCard city="서울" />
+        <div css={styles.cards}>
+          {cities.map((city) => (
+            <HomeFestivalCard key={city.id} city={city.city} />
+          ))}
+        </div>
       </section>
     </div>
   );
 };
+
 export default Home;
