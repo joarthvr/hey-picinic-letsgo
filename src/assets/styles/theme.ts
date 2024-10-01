cf6e3145bbeef5b121cd2e6279a8f3fcdf38d5ad
Arthur Jo <ioi456859@gmail.com>
2024-10-01 14:39:59 +0900

    style: list page style3

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -5,6 +5,22 @@ export const theme = {
     TEXT_DARKGRAY: '#393939',
     TEXT_LIGHTGRAY: '#626262',
   },
+  background: {
+    listPageTop: {
+      height: '20.375rem',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+      fontFamily: 'Pretendard',
+    },
+  },
+  position: {
+    center: {
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  },
   fonts: {
     header: {
       fontFamily: 'Pretendard',
@@ -18,6 +34,27 @@ export const theme = {
       fontSize: '0.75rem',
       fontWeight: '300',
       lineHeight: '1.5',
+    },
+    listPageH1: {
+      color: '#FFF',
+      fontSize: '2.5rem',
+      fontStyle: 'normal',
+      fontWeight: 800,
+      lineHeight: 'normal',
+    },
+    listPageH1Eng: {
+      color: 'rgba(255, 255, 255, 0.52)',
+      fontSize: '1.25rem',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: 'normal',
+    },
+    listPageH2: {
+      color: '#000',
+      fontSize: '1.5rem',
+      fontStyle: 'normal',
+      fontWeight: '400',
+      lineHeight: 'normal',
     },
   },
   icons: {
@@ -65,11 +102,6 @@ export const theme = {
       fontWeight: '600',
       lineHeight: '1rem',
       whiteSpace: 'nowrap',
-    },
-  },
-  img: {
-    listTopImg: {
-      width: '100%',
     },
   },
 };
--- src/pages/FestivalList.tsx
+++ src/pages/FestivalList.tsx
@@ -3,46 +3,27 @@ import { css, useTheme } from '@emotion/react';
 import { useMemo } from 'react';
 import { ThemeType } from '../assets/styles/theme';
 const festivalListStyle = (theme: ThemeType) => ({
-  container: css({
-    color: '#fff',
-    fontFamily: 'Pretendard',
-    ...theme.interval.width,
-    marginTop: '5.69rem',
-    paddingBottom: '10.75rem',
-  }),
   section1: css({
-    width: '100%',
-    height: '20.375rem',
     backgroundImage: `url(${FestivalImg})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center center',
-    backgroundSize: 'cover',
     position: 'relative',
+    ...theme.background.listPageTop,
   }),
   sec1TextBox: css({
     position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    ...theme.position.center,
   }),
   h1: css({
-    margin: 'auto',
-    textAlign: 'center',
-    color: '#FFF',
-    fontFamily: 'Pretendard',
-    fontSize: '2.5rem',
-    fontStyle: 'normal',
-    fontWeight: 800,
-    lineHeight: 'normal',
+    ...theme.fonts.listPageH1,
   }),
-  sec1P: css({
-    textAlign: 'center',
-    color: 'rgba(255, 255, 255, 0.52)',
+  h1Eng: css({
+    ...theme.fonts.listPageH1Eng,
+  }),
+  section2: css({
     fontFamily: 'Pretendard',
-    fontSize: '1.25rem',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    lineHeight: 'normal',
+    ...theme.interval.width,
+  }),
+  h2: css({
+    ...theme.fonts.listPageH2,
   }),
 });
 const FestivalList = () => {
@@ -53,11 +34,13 @@ const FestivalList = () => {
       <section css={styles.section1}>
         <div css={styles.sec1TextBox}>
           <h1 css={styles.h1}>행사</h1>
-          <p css={styles.sec1P}>Festival</p>
+          <p css={styles.h1Eng}>Festival</p>
         </div>
       </section>
-      <section>
-        <h2>서울시의 서울 뮤직페스티벌에 대한 검색 결과입니다.</h2>
+      <section css={styles.section2}>
+        <h2 css={styles.h2}>
+          서울시의 서울 뮤직페스티벌에 대한 검색 결과입니다.
+        </h2>
       </section>
     </>
   );
